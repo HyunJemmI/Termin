40aef3f46dcc27b5336ad3922684b0f8b9ddc0ae
Hyunje <81470469+HyunJemmI@users.noreply.github.com>
2025-05-20 15:38:59 +0900

    Update request.py

--- request.py
+++ request.py
@@ -30,8 +30,8 @@ def send_email():
     app_password = os.getenv("EMAIL_APP_PASSWORD") # 패스워드 <- 진짜진짜진짜 env에 넣어서 불러오려무나
 
     # ────────────────────────────────
-    # EMAIL_APP_PASSWORD=너 이메일 비밀번호 // 어차피 위에 ""로 묶여있어서 따로 하지마
-    # EMAIL_SENDER = 님 이메일. 남자친구가 챙겨주는 느낌 내고싶으면 남자친ㄹ구 이메일 넣으렴
+    # EMAIL_APP_PASSWORD=너 이메일 비밀번호 // 어차피 위에 ""로 묶여있어서 따로 할 필요 X
+    # EMAIL_SENDER = 님 이메일.
     # EMAIL_RECEIVER = 님 이메일
     # ────────────────────────────────  
     subject = "뮌헨 예약 ㄱㄱㄱㄱㄱㄱㄱㄱㄱ"
